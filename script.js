faf41245099abbcd5a5b9605976979fe62ca6503
Steven-Ou <osteve425@gmail.com>
2024-11-16 21:28:38 -0500

    connected

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let num = 
+let num = document.getElementById("amount"); 
+
 function printMessage(num){
     let b = Math.random()%8;
     let r = Math.random()%8;
-    for(let i =9; i<) 
+    for(let i =9; i<num; i++){
+        
+    } 
 }
