1ab65a7005a4ad294d7418feb20ad33003b3abf9
Steven-Ou <osteve425@gmail.com>
2025-01-03 19:41:16 +0800

    first color

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let num = document.getElementById("amount"); 
 let data;//How should i import colors or should i say i dont need it hmmm tbc... 
 const colors = [
-
+    "\x1b[31m",
 ];
 
 function printMessage(num){
