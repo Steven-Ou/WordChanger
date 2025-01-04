a96c55db258d5aeb96b5cfb2a5c7934dbabaab56
Steven-Ou <osteve425@gmail.com>
2025-01-04 14:37:07 +0800

    comment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let num = document.getElementById("amount"); 
 let data;//How should i import colors or should i say i dont need it hmmm tbc... 
 const colors = [
-    "\x1b[31m",
+    "\x1b[31m",//Red
     "\x1b[33m",
     "\x1b[32m",
     "\x1b[36m",
