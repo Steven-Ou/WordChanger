85205f349836ddf08e1a42a704362d393e0f6a61
Steven-Ou <osteve425@gmail.com>
2025-01-08 11:52:35 +0800

    coding out the colors again

--- script.js
+++ script.js
@@ -29,3 +29,14 @@ const prompt = require('prompt-sync')();
 const message = prompt("ENTER A MESSAGE! :");
 printMessage(message);
 */
+
+const colors = [
+    "\x1b[31m",//Red
+    "\x1b[33m",//Yellow
+    "\x1b[32m",//Green
+    "\x1b[36m",//Cyan
+    "\x1b[34m",//Blue
+    "\x1b[35m",//Purple
+    "\x1b[30m",//Black
+    "\x1b[37m"//White
+];
