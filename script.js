a42c266a58eabbf6514e87a58294d3aa9619b48b
Steven-Ou <osteve425@gmail.com>
2025-01-08 11:52:58 +0800

    removing

--- script.js
+++ script.js
@@ -32,11 +32,5 @@ printMessage(message);
 //Method of combining HTML CSS AND JS
 const colors = [
     "\x1b[31m",//Red
-    "\x1b[33m",//Yellow
-    "\x1b[32m",//Green
-    "\x1b[36m",//Cyan
-    "\x1b[34m",//Blue
-    "\x1b[35m",//Purple
-    "\x1b[30m",//Black
-    "\x1b[37m"//White
+   
 ];
