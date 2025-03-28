ecc0b146d660d787ea7458225f22069e5765d389
Steven-Ou <osteve425@gmail.com>
2025-03-27 21:19:30 -0400

    fixing and improvising

--- word/app/page.js
+++ word/app/page.js
@@ -1,10 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Input } from "@mui/material";
 
 export default function Home() {
   return (
     <div>
       <Box>
-        
+      <h1 class="change">WELCOME TO WORD COLOR CHANGER</h1>
+  <div id="lock">
+    <h2 id="as">Something Cool Will Happen!</h2>
+    <div class="ab">
+      <Input type="text" id="amount" placeholder="Enter a sentence" style="text-align:center;"></Input>
+      <button onclick="printMessage()">Enter</button>
+    </div>
+    <h3 style="text-decoration: underline;">Output:</h3>
+    <div id="output"></div>
+  </div>
       </Box>
     </div>
   );
