14a955ceaee455827c095dca6c845fa4188259ee
Steven-Ou <osteve425@gmail.com>
2025-03-27 21:22:41 -0400

    Improvising still

--- word/app/page.js
+++ word/app/page.js
@@ -5,15 +5,11 @@ export default function Home() {
     <div>
       <Box>
       <h1 className="change">WELCOME TO WORD COLOR CHANGER</h1>
-  <div id="lock">
     <h2 id="as">Something Cool Will Happen!</h2>
     <div class="ab">
       <Input type="text" id="amount" placeholder="Enter a sentence" style={{textAlign:"center"}}></Input>
       <Button></Button>
     </div>
-    <h3 style="text-decoration: underline;">Output:</h3>
-    <div id="output"></div>
-  </div>
       </Box>
     </div>
   );
