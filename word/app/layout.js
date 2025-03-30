b6c3402286a5e50e693f1cdea795741cae4b6d1f
Steven-Ou <osteve425@gmail.com>
2025-03-30 11:49:03 -0400

    updating

--- word/app/layout.js
+++ word/app/layout.js
@@ -5,6 +5,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
+        
       </body>
     </html>
   );
--- word/app/page.js
+++ word/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Box>
-      <h1 className="change">WELCOME TO WORD COLOR CHANGER</h1>
+    
     <h2 id="as">Something Cool Will Happen!</h2>
     <div class="ab">
       <Input type="text" id="amount" placeholder="Enter a sentence" style={{textAlign:"center"}}></Input>
