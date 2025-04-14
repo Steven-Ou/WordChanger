330004099f39033ac9239f29bbb4d3cd2e84c262
Steven-Ou <osteve425@gmail.com>
2025-04-14 12:05:00 -0400

    updating with tailwind

--- word/app/page.js
+++ word/app/page.js
@@ -32,17 +32,20 @@ export default function Home() {
   
   return (
     //Frontend Section:
-    <div className="rounded-xl">
-      <Box id = "lock">
+    <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white 
+    p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 
+    dark:shadow-none dark:outline-white/10
+    ">
+      <Box id = "lock" className="flex flex-col gap-y-4 text-colors-white-500 dark:text-white-400">
         <h1 className="change">WELCOME TO WORD COLOR CHANGER</h1>
         <br></br>
-        <h2 id="as">Something Cool Will Happen!</h2>
+        <h2 id="as" className=" text-white-500 dark:text-white-400">Something Cool Will Happen!</h2>
         <br></br>
         <br></br>
         <div className="ab">
           <Input 
           type="text" 
-          id="amount" 
+          id="amount text-white-500 dark:text-white-400" 
           placeholder="Enter a sentence" 
           style={{textAlign:"center"}} 
           value={inP} 
