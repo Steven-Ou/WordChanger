7ab02bd05b0f81d23651aad37487ae88c79c9a87
Steven-Ou <osteve425@gmail.com>
2025-04-16 01:20:33 -0400

    centeralizing the words

--- word/app/page.js
+++ word/app/page.js
@@ -56,7 +56,7 @@ export default function Home() {
               opacity:1,
             },
             input:{
-              
+              textAlign:"center",
             }
           }}
           >
