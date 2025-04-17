893ea1402ba544caaed8c9f5c9254f6c26f9c829
Steven-Ou <osteve425@gmail.com>
2025-04-17 00:16:44 -0400

    updating the box the welcome

--- word/app/page.js
+++ word/app/page.js
@@ -33,14 +33,14 @@ export default function Home() {
   return (
     //Frontend Section:
     <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-3xl bg-green-200 
-    p-6 shadow-lg outline outline-black/5 border-4
+    p-6 shadow-lg outline outline-black/5 
     dark:shadow-none dark:outline-white/10
     ">
       <Box id = "lock" className="flex flex-col gap-y-4 text-colors-white-500 dark:text-white-400
         rounded-3xl p-6 outline outline-green-800
         dark:outline-green-800 dark:outline-1 border-4
       ">
-        <h1 className="change">WELCOME TO WORD COLOR CHANGER</h1>
+        <h1 className="change animate-borderColor border-4 rounded-3xl">WELCOME TO WORD COLOR CHANGER</h1>
         <br></br>
         <h2 id="as" className="text-gray-800 dark:text-gray-500">Something Cool Will Happen!</h2>
         <br></br>
@@ -51,7 +51,7 @@ export default function Home() {
           id="amount" 
           placeholder="Enter a sentence" 
           autoComplete="off"
-          className="transition-all duration-300 ease-in-out border-4 rounded-2xl animate-borderColor"
+          className="transition-all duration-300 ease-in-out"
           style={{textAlign:"center"}} 
           value={inP} 
           onChange={(e) => setinP(e.target.value)}
