e0f1d91d5c6963d7bbb9c59e58155b26e6e729d2
Steven-Ou <osteve425@gmail.com>
2025-04-17 00:28:19 -0400

    making the background color animated

--- word/app/layout.js
+++ word/app/layout.js
@@ -3,7 +3,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="mx-auto flex items-center rounded 3xl bg-green-500 p-6 shadow-lg outline outline-black/5 dark:shadow-none dark:outline-white/10">
+      <body className="mx-auto flex items-center bg-green-500 p-6 shadow-lg outline
+      animate-backgroundColor
+      ">
         {children}
       </body>
     </html>
