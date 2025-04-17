84400a593b64a11d428d6d8bd6bf2eb7fe4b7451
Steven-Ou <osteve425@gmail.com>
2025-04-17 00:12:49 -0400

    making the box look more thicker

--- word/app/page.js
+++ word/app/page.js
@@ -33,12 +33,12 @@ export default function Home() {
   return (
     //Frontend Section:
     <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-3xl bg-green-200 
-    p-6 shadow-lg outline outline-black/5 
+    p-6 shadow-lg outline outline-black/5 border-4
     dark:shadow-none dark:outline-white/10
     ">
       <Box id = "lock" className="flex flex-col gap-y-4 text-colors-white-500 dark:text-white-400
         rounded-3xl p-6 outline outline-green-800
-        dark:outline-green-800 dark:outline-1
+        dark:outline-green-800 dark:outline-1 border-4
       ">
         <h1 className="change">WELCOME TO WORD COLOR CHANGER</h1>
         <br></br>
