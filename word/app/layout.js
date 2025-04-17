e83c97fcb87d540ec454ef0cbcf1a03801e01d94
Steven-Ou <osteve425@gmail.com>
2025-04-16 23:53:08 -0400

    updating the color

--- word/app/layout.js
+++ word/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="rounded 3xl bg-green-200">
+      <body className="mx-auto flex items-center rounded 3xl bg-green-500 ">
         {children}
       </body>
     </html>
