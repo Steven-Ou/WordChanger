c4785d4aca0a8a5300e4282fc5c25de745be082d
Steven-Ou <osteve425@gmail.com>
2025-04-18 23:35:18 -0400

    mkaing the word thicker

--- word/app/page.js
+++ word/app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
         ">
           WELCOME TO WORD COLOR CHANGER
         </h1>
-        <h2 id="as" className="text-gray-800 dark:text-gray-500">
+        <h2 id="as" className="text-gray-800 dark:text-gray-500 font-bold">
           Something Cool Will Happen!
         </h2>
         <div className="ab">
